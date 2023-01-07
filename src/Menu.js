9c3877d877b406e75e8b99fff6fd99a1b766a515
Renato <renatossnt@gmail.com>
2023-01-07 17:55:32 -0300

    select algoritmo funcionando

--- src/Menu.js
+++ src/Menu.js
@@ -2,17 +2,31 @@ import React from "react";
 import "./Menu.css";
 
 const Menu = ({ clearWalls, resetGrid, runAlgorithm }) => {
+  const [algorithm, setAlgorithm] = React.useState("bfs");
+
+  function selectAlgorithm() {}
+
   return (
     <div className="menu">
-      <button onClick={resetGrid}>Reset Grid</button>
-      <button onClick={clearWalls}>Clear Walls</button>
-      <select>
-        <option>Dijkstra</option>
-        <option>DFS</option>
-        <option>BFS</option>
+      <button className="reset-btn" onClick={resetGrid}>
+        Reset Grid
+      </button>
+      <button className="clear-btn" onClick={clearWalls}>
+        Clear Walls
+      </button>
+
+      <select
+        value={algorithm}
+        onChange={({ target }) => setAlgorithm(target.value)}
+      >
+        <option value="dfs">DFS</option>
+        <option value="bfs">BFS</option>
+        <option value="dijkstra">Dijkstra</option>
       </select>
-      <button onClick={() => runAlgorithm("dfs")}>Run</button>
-      <button>Help</button>
+      <button className="run-btn" onClick={() => runAlgorithm(algorithm)}>
+        Run
+      </button>
+      <button className="help-btn">Help</button>
     </div>
   );
 };
