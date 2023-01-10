e65896958fdac656105dc06a3f50b876df699fc2
Renato <renatossnt@gmail.com>
2023-01-10 17:45:20 -0300

    add favicon

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "./img/logo.png";
 
 const Header = () => {
   return (
     <header className="header">
+      <img src={logo}></img>
       <h1 className="title">Pathfinding Visualizer</h1>
     </header>
   );
